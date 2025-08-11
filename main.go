c3bcfca954e6f71660848932eefb25fdd08e19e8
devfernandoa <fefealzueta@gmail.com>
2025-08-11 19:57:20 -0300

    v0.0.7

--- main.go
+++ main.go
@@ -1,91 +1,76 @@
 // main.go
 package main
 
-import(
-    "fmt"
-    "os"
-    "strconv"
-    "unicode"
+import (
+	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-    if len(os.Args) != 2 {
-        fmt.Fprintln(os.Stderr, "Uso: go run main.go 'conta'")
-        os.Exit(1)
-    }
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "Uso: go run main.go 'conta'")
+		os.Exit(1)
+	}
 
-    cadeia:= os.Args[1]
-    cadeiaSemEspacos:= ""
-    for _, c := range cadeia {
-        if c != ' ' {
-            cadeiaSemEspacos += string(c)
-        }
-    }
+	expr := os.Args[1]
+	expr = removeSpaces(expr)
+	if expr == "" {
+		fmt.Fprintln(os.Stderr, "Erro: input inválido")
+		os.Exit(1)
+	}
 
-    // Mantém apenas dígitos e +-
-    aux:= ""
-    for _, c := range cadeiaSemEspacos {
-        if unicode.IsDigit(c) || c == '+' || c == '-' {
-            aux += string(c)
-        }
-    }
-    cadeiaSemEspacos = aux
+	res, err := evalSimple(expr)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Erro: input inválido")
+		os.Exit(1)
+	}
+	fmt.Println(res)
+}
 
-    if len(cadeiaSemEspacos) == 0 {
-        fmt.Fprintln(os.Stderr, "Erro: input inválido")
-        os.Exit(1)
-    }
+func removeSpaces(s string) string {
+	out := ""
+	for _, c := range s {
+		if c != ' ' {
+			out += string(c)
+		}
+	}
+	return out
+}
 
-    var listaOperacoes []rune
-    var listaNumeros []int
-    num:= ""
-
-    for i, c := range cadeiaSemEspacos {
-        if c == '+' || c == '-' {
-            if num == "" || i == 0 || i == len(cadeiaSemEspacos) - 1 {
-                fmt.Fprintln(os.Stderr, "Erro: input inválido")
-                os.Exit(1)
-            }
-            listaOperacoes = append(listaOperacoes, c)
-            if num != "" {
-                n, err := strconv.Atoi(num)
-                if err != nil {
-                    fmt.Fprintln(os.Stderr, "Erro: input inválido")
-                    os.Exit(1)
-                }
-                listaNumeros = append(listaNumeros, n)
-                num = ""
-            }
-        } else {
-            num += string(c)
-        }
-    }
-    if num != "" {
-        n, err := strconv.Atoi(num)
-        if err != nil {
-            fmt.Fprintln(os.Stderr, "Erro: input inválido")
-            os.Exit(1)
-        }
-        listaNumeros = append(listaNumeros, n)
-    }
-
-    if len(listaNumeros) == 0 || len(listaOperacoes) == 0 {
-        fmt.Fprintln(os.Stderr, "Erro: input inválido")
-        os.Exit(1)
-    }
-
-    resultado:= 0
-    for i, n := range listaNumeros {
-        if i == 0 {
-            resultado = n
-        } else {
-            if listaOperacoes[i - 1] == '+' {
-                resultado += n
-            } else {
-                resultado -= n
-            }
-        }
-    }
-
-    fmt.Println(resultado)
+func evalSimple(expr string) (int, error) {
+	n := len(expr)
+	if n == 0 {
+		return 0, fmt.Errorf("vazio")
+	}
+	res := 0
+	num := ""
+	op := byte('+')
+	for i := 0; i < n; i++ {
+		c := expr[i]
+		if c >= '0' && c <= '9' {
+			num += string(c)
+		}
+		if c == '+' || c == '-' || i == n-1 {
+			if num == "" {
+				return 0, fmt.Errorf("input inválido")
+			}
+			val, err := strconv.Atoi(num)
+			if err != nil {
+				return 0, err
+			}
+			if op == '+' {
+				res += val
+			} else {
+				res -= val
+			}
+			op = c
+			num = ""
+		}
+	}
+	// Se terminou com operador, erro
+	if num == "" && (expr[n-1] == '+' || expr[n-1] == '-') {
+		return 0, fmt.Errorf("input inválido")
+	}
+	return res, nil
 }
