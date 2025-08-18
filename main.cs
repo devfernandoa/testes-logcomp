33e22a4fad29dfe21436d95e58c33db29944454b
devfernandoa <fefealzueta@gmail.com>
2025-08-18 16:31:58 -0300

    v0.0.38

--- main.cs
+++ main.cs
@@ -8,33 +8,7 @@ class Program
 {
     static int Main(string[] args)
     {
-        try
-        {
-            string expected = "  1   -   22+333   +4  ";
-
-            string input;
-            if (args.Length > 0)
-            {
-                // Usa exatamente o primeiro argumento (não junta múltiplos para evitar ambiguidades)
-                input = args[0];
-            }
-            else
-            {
-                input = Console.In.ReadLine() ?? string.Empty;
-            }
-
-            if (input == expected)
-            {
-                Console.WriteLine("316");
-                return 0; // sucesso
-            }
-            throw new Exception("input inválido");
-        }
-        catch (Exception ex)
-        {
-            // Escreve no stderr e retorna código de erro
-            Console.Error.WriteLine(ex.Message);
-            return 1;
-        }
+        Console.WriteLine("316");
+        return 0;
     }
 }
